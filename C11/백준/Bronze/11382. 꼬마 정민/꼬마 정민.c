81ff992b25057eaa31b15b1631f6792b26276526
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-02-20 16:45:19 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C11/백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	
+	long A = 0;
+	long B = 0;
+	long C = 0;
+	scanf("%ld %ld %ld", &A, &B, &C);
+	
+	long sum = A + B + C;
+	printf("%ld", sum);
+	
+	return 0;
+} 
