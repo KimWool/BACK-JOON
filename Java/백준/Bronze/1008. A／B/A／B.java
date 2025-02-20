4b6afbf71f9972e91fb81f38535f6a0f18355f8e
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-02-20 16:28:36 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17764 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        double A = s.nextInt();
+        double B = s.nextInt();
+
+        double div = A / B;
+        System.out.println(div);
+
+    }
+}
