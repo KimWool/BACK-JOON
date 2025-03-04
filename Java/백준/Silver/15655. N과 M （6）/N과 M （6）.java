82da32ca3ed08fd9552975d8042369d806019caa
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-03-04 13:16:43 +0900

    [Silver III] Title: N과 M (6), Time: 192 ms, Memory: 18396 KB -BaekjoonHub

---
+++ Java/백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt(); //N개의 자연수 중에서
+        int M = s.nextInt(); //M개를 고른 수열
+
+        int[] array = new int[N + 1];
+        boolean[] visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            array[i] = s.nextInt();
+        }
+
+        Arrays.sort(array);
+        int[] arr = new int[M];
+
+        backtrack(N, M, 0, array, arr, visited);
+
+    }
+
+    public static void backtrack(int n, int m, int depth, int[] array, int[] arr, boolean[] visited) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                for (int j = i; j >= 1; j--){
+                    visited[j] = true;
+                }
+                arr[depth] = array[i];
+                backtrack(n, m, depth + 1, array, arr, visited);
+                for (int j = i; j >= 1; j--){
+                    visited[j] = false;
+                }
+            }
+        }
+    }
+}
