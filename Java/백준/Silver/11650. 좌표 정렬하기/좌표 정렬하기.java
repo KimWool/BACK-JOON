03cc16f94dc2b076101238c3931ac30ac8a94011
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-04-15 21:48:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1396 ms, Memory: 64136 KB -BaekjoonHub

---
+++ Java/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static class Point{
+        int x;
+        int y;
+        Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N;
+        try {
+            N = Integer.parseInt(br.readLine()); //점의 개수
+            Point[] points = new Point[N];
+            for (int i = 0; i < N; i++) {
+                String[] input = br.readLine().split(" ");
+                int x = Integer.parseInt(input[0]);
+                int y = Integer.parseInt(input[1]);
+                points[i] = new Point(x, y);
+            }
+
+            Arrays.sort(points, Comparator.comparingInt((Point p) -> p.x).thenComparingInt(p -> p.y));
+
+            for (Point p : points) {
+                System.out.println(p.x + " " + p.y);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
