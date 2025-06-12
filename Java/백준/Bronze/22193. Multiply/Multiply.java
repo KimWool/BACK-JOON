3906e8a1bfc982d5c8653ff5a789fae26d6a8a25
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-06-12 22:05:12 +0900

    [Bronze V] Title: Multiply, Time: 568 ms, Memory: 54484 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Bronze/22193. Multiply/Multiply.java
+++ Java/백준/Bronze/22193. Multiply/Multiply.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,9 +12,9 @@ public class Main {
         long N = Long.parseLong(input[0]);
         long M = Long.parseLong(input[1]);
 
-        int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
+        BigInteger A = new BigInteger(br.readLine());
+        BigInteger B = new BigInteger(br.readLine());
 
-        System.out.print(A * B);
+        System.out.print(A.multiply(B));
     }
 }
