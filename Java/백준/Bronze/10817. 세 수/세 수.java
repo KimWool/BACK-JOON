f7ed44739cc873f5d2b46ade6a08647f47b08a61
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-06-15 18:06:20 +0900

    [Bronze IV] Title: 세 수, Time: 112 ms, Memory: 14296 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int[] numbers = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(numbers);
+        System.out.print(numbers[1]);
+    }
+}
