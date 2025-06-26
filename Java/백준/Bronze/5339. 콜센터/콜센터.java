a34372807274d196bc99aa63d200463ca5f86c75
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-06-26 13:41:21 +0900

    [Bronze V] Title: 콜센터, Time: 100 ms, Memory: 13904 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("     /~\\\n");
+        System.out.print("    ( oo|\n");
+        System.out.print("    _\\=/_\n");
+        System.out.print("   /  _  \\\n");
+        System.out.print("  //|/.\\|\\\\\n");
+        System.out.print(" ||  \\ /  ||\n");
+        System.out.print("============\n");
+        System.out.print("|          |\n");
+        System.out.print("|          |\n");
+        System.out.print("|          |");
+    }
+}
