70f59eb4154a1aa448ae971b16a622050ba2f88e
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-08-20 14:24:38 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int towerHeight = Integer.parseInt(br.readLine());
+
+        BigInteger result = BigInteger.ONE.shiftLeft(towerHeight);
+        System.out.println(result);
+    }
+}
