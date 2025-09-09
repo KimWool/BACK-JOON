fc68c8507a39b0175cff964d7947edc8b131b97f
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-09-09 13:31:16 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ Java/백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+
+        int count = 1;
+
+        while (B > A) {
+            if (B % 2 == 0) {
+                B /= 2;
+            } else if (B % 10 == 1) {
+                B /= 10;
+            } else {
+                break;
+            }
+            count++;
+        }
+
+        if (B == A) {
+            System.out.println(count);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
