86fb0005d690ed671b5511490cc39039d2183ba1
katasan0410 <84843984+KimWool@users.noreply.github.com>
2025-11-11 21:19:38 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 204 ms, Memory: 18028 KB -BaekjoonHub

---
+++ Java/백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] r = new int[N];
+        int[] c = new int[N];
+        int[][] dp = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            r[i] = Integer.parseInt(st.nextToken());
+            c[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int len = 1; len < N; len++) {
+            for (int i = 0; i + len < N; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j],
+                        dp[i][k] + dp[k+1][j] + r[i] * c[k] * c[j]);
+                }
+            }
+        }
+
+        System.out.println(dp[0][N-1]);
+    }
+}
